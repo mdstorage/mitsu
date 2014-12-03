ec8d4f3c66ecdbf67c9924bea916b1e8215c49a6
Misha <m@m.m>
2014-12-03 16:34:35 +0400

    создал CatalogMazdaBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Catalog\MitsubishiBundle\CatalogMitsubishiBundle(),
+            new Catalog\MazdaBundle\CatalogMazdaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
