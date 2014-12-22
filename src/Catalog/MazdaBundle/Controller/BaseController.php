e5593670601073ef7c9c123151d1aad2c6f6eaf2
MishaHome <misha@example.com>
2014-12-22 19:40:57 +0300

    clear base controller

--- src/Catalog/MazdaBundle/Controller/BaseController.php
+++ src/Catalog/MazdaBundle/Controller/BaseController.php
@@ -12,22 +12,5 @@ namespace Catalog\MazdaBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BaseController extends Controller{
-    public $params = array();
 
-    public function getParams()
-    {
-        return $this->params;
-    }
-
-    protected function addParam($name, $value)
-    {
-        $params = $this->getParams();
-        $params[$name] = $value;
-        $this->params = $params;
-    }
-
-    protected function clearParams()
-    {
-        $this->params = array();
-    }
 } 
