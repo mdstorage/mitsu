617efb94ff9b9197f0096f3e3885831ccafd8561
MishaHome <misha@example.com>
2015-01-11 18:45:33 +0300

    before subaru

--- src/Catalog/CommonBundle/Controller/ArticulController.php
+++ src/Catalog/CommonBundle/Controller/ArticulController.php
@@ -2,6 +2,31 @@
 namespace Catalog\CommonBundle\Controller;
 
 
-class ArticulController extends BaseController{
+use Symfony\Component\HttpFoundation\Request;
 
+abstract class ArticulController extends CatalogController{
+
+    public function indexAction()
+    {
+        return $this->render($this->bundle().':01_index.html.twig');
+    }
+
+    public function findByArticulAction(Request $request, $regionCode = null)
+    {
+        $articul = $request->get('articul');
+        $articulRegions = $this->model()->getArticulRegions($articul);
+        $articulModels  = $this->model()->getArticulModels($articul);
+
+        $this->addFilter('aticulModelsFilter', array(
+            'articulRegions' => $articulRegions,
+            'articulModels'  => $articulModels
+        ));
+
+        return $this->regionsModelsAction($regionCode);
+    }
+
+    public function aticulRegionModelsFilter($oContainer, $parameters)
+    {
+
+    }
 } 
--- src/Catalog/MazdaBundle/Controller/ArticulController.php
+++ src/Catalog/MazdaBundle/Controller/ArticulController.php
@@ -2,8 +2,22 @@
 namespace Catalog\MazdaBundle\Controller;
 
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Catalog\CommonBundle\Controller\ArticulController as BaseController;
 
-class ArticulController extends Controller{
+class ArticulController extends BaseController{
 
+    public function bundle()
+    {
+        return 'CatalogMazdaBundle:Articul';
+    }
+
+    public function model()
+    {
+        return $this->get('mazda.articul.model');
+    }
+
+    public function bundleConstants()
+    {
+        return 'Catalog\MazdaBundle\Components\MazdaConstants';
+    }
 } 
