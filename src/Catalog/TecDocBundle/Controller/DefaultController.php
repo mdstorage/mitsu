bab9d64834d5a3d1c827f9c8e8e4904814c4bcb5
mail <mail@gengen.by>
2015-01-22 10:19:36 +0000

    in0

--- src/Catalog/TecDocBundle/Controller/DefaultController.php
+++ src/Catalog/TecDocBundle/Controller/DefaultController.php
@@ -11,7 +11,15 @@ class DefaultController extends Controller
         $client = new \SoapClient('http://webservice-cs.tecdoc.net/pegasus-2-0/wsdl/TecdocToCatWL');
 
       //$client->addDynamicAddress('93.85.95.12', 20078, 4);
-        $result = $client->getVehicleManufacturers3(1, null, false, false, 1, 'ru', 20078);
+        $result = $client->getVehicleManufacturers2(array('in0' =>array(
+            'carType' => '1',
+            'countriesCarSelection' => 'ru',
+            'countryGroupFlag' => false,
+            'evalFavor' => false,
+            'favouredList' => 1,
+            'lang' => 'ru',
+            'provider' => 20078))
+        );
 
 var_dump($result);die;
         return $this->render('CatalogTecDocBundle:Default:index.html.twig', array('name' => $result));
