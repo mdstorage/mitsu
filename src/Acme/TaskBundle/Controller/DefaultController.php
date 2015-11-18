c6723f4e1f65d1bf52e95ce669b9b722ac75e4d4
mdstorage <mail@gengen.by>
2015-11-18 16:53:31 +0300

    добавлен мини купер

--- src/Acme/TaskBundle/Controller/DefaultController.php
+++ src/Acme/TaskBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $marks = array('Huyndai', 'KIA', 'Honda', 'Suzuki', 'Subaru', 'Mazda', 'Mercedes', 'BMV');
+        $marks = array('Huyndai', 'KIA', 'Honda', 'Suzuki', 'Subaru', 'Mazda', 'Mercedes', 'BMV', 'Mini');
         return $this->render('AcmeTaskBundle:Default:index.html.twig', array('marks' => $marks));
     }
 }
