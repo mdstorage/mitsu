9d0e06ecec1c81ac187f0ce1f7254be418afa65a
mdstorage <mail@gengen.by>
2016-02-04 15:02:35 +0300

    фольксваген. первый коммит

--- src/Catalog/VolkswagenBundle/Models/VolkswagenCatalogModel.php
+++ src/Catalog/VolkswagenBundle/Models/VolkswagenCatalogModel.php
@@ -160,7 +160,7 @@ class VolkswagenCatalogModel extends CatalogModel{
         foreach($aGroup as $item){
 
             $groups[$item['hg']=='0'?'10':$item['hg']] = array(
-                Constants::NAME     => mb_strtoupper(iconv('Windows-1251', 'UTF-8', $item ['text']),'utf8'),
+                Constants::NAME     => iconv('Windows-1251', 'UTF-8', $item ['text']),
                 Constants::OPTIONS  => array()
             );
         }
