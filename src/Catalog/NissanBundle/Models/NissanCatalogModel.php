061f2fe6296d4e2e00ad1ddfd7f1e2fbf70174ed
mdstorage <mail@gengen.by>
2016-03-18 18:42:36 +0300

    nissan. все хорошо с кодировкой

--- src/Catalog/NissanBundle/Models/NissanCatalogModel.php
+++ src/Catalog/NissanBundle/Models/NissanCatalogModel.php
@@ -601,7 +601,7 @@ class NissanCatalogModel extends CatalogModel{
 
 
 
-               $pncs[$item['PARTCODE']][Constants::NAME] = $item['PARTNAME_E'];
+               $pncs[$item['PARTCODE']][Constants::NAME] = iconv('utf8', 'Shift-JIS', $item['PARTNAME_E']);
 
 
 
