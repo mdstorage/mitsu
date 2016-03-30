7d9140ebbe1ea0a4aa5b2224f70929275f869003
mdstorage <mail@gengen.by>
2016-03-30 17:36:42 +0300

    ниссан.инфинити.новые фильтры при поиске по артикулу

--- src/Catalog/InfinitiBundle/Controller/Traits/InfinitiArticulFilters.php
+++ src/Catalog/InfinitiBundle/Controller/Traits/InfinitiArticulFilters.php
@@ -36,49 +36,62 @@ trait InfinitiArticulFilters {
 
 
         $aAll = array();
+
+        $aAllarticulDesc = array();
+
+
         $minLen = 10;
 
         /*если имеют место разные ограничения для одного и того же артикула,
-        то выбирается ограничение с меньшей длиной строки. это спорно, но пока так*/
+        то все они объединяются в один массив и учитывается каждое из них*/
 
         foreach ($articulDesc as $index => $value)
         {
-            if (strlen($value['REC3']) < $minLen)
-            {
-                $minLen = strlen($value['REC3']);
-            }
+            $aAll[] = $value['REC3'];
 
         }
+        $aAllarticulDesc = (implode(' +', $aAll));
 
-        foreach ($articulDesc as $index => $value)
-        {
-            if (strlen($value['REC3']) != $minLen)
-            {
-                unset ($articulDesc[$index]);
-            }
-        }
+
 
         /*для найденных ограничений на использование артикулов в различных комплектациях (массив $articulDesc)
         производятся сравнения со всеми комплектациями (массив $complectations). Если описание применяемости для запчасти (таблица catalog, столбец REC3)
          и комплектация не совпадают, то комплектация не попадает в выдачу
          * */
 
+
+
         foreach ($complectations as $indexC => $valueC) {
 
-
-            foreach ($articulDesc as $index => $value) {
                 $ct = 0;
-                $schemaOptions = $this->model()->multiexplode(array('+', ' +', '+ '), $value['REC3']);
+                $schemaOptions = $this->model()->multiexplode(array(' +'), $aAllarticulDesc);
+
+
 
 
                 foreach ($schemaOptions as $item) {
 
-                    $item = trim($item, ('*()'));
+
+
+
+
                     if (strpos($item, ".")) {
+
                         $plus = explode('.', $item);
 
 
-                        if (count($plus) == count(array_intersect($plus, explode('.', $valueC->getOptions()['OPTION9'])))) {
+                        $plusExplode = $this->model()->multiexplode(array('.','+'), $item);
+
+
+
+                        foreach ($plusExplode as &$item)
+                        {
+                            $item = trim($item, ('*()'));
+                        }
+
+
+
+                        if (count($plus) === count(array_intersect($plusExplode, explode('.', $valueC->getOptions()['OPTION9'])))) {
                             $ct = $ct + 1;
                         }
 
@@ -99,7 +112,7 @@ trait InfinitiArticulFilters {
                     $oContainer->getActiveModification()->removeComplectation($indexC);
                 }
 
-            }
+
         }
 
 
--- src/Catalog/NissanBundle/Controller/Traits/NissanArticulFilters.php
+++ src/Catalog/NissanBundle/Controller/Traits/NissanArticulFilters.php
@@ -30,82 +30,93 @@ trait NissanArticulFilters {
     public function CFilter($oContainer, $parameters)
     {
 
-       $articulDesc = $this->model()->getArticulDesc($parameters['articul'], $parameters['regionCode'], $parameters['modelCode'], $parameters['modificationCode']);
+        $articulDesc = $this->model()->getArticulDesc($parameters['articul'], $parameters['regionCode'], $parameters['modelCode'], $parameters['modificationCode']);
 
         $complectations = $oContainer->getActiveModification()->getComplectations();
 
 
         $aAll = array();
+
+        $aAllarticulDesc = array();
+
+
         $minLen = 10;
 
         /*если имеют место разные ограничения для одного и того же артикула,
-        то выбирается ограничение с меньшей длиной строки. это спорно, но пока так*/
+        то все они объединяются в один массив и учитывается каждое из них*/
 
         foreach ($articulDesc as $index => $value)
         {
-            if (strlen($value['REC3']) < $minLen)
-            {
-                $minLen = strlen($value['REC3']);
-            }
+            $aAll[] = $value['REC3'];
 
         }
+        $aAllarticulDesc = (implode(' +', $aAll));
 
-        foreach ($articulDesc as $index => $value)
-        {
-            if (strlen($value['REC3']) != $minLen)
-            {
-                unset ($articulDesc[$index]);
-            }
-        }
+
 
         /*для найденных ограничений на использование артикулов в различных комплектациях (массив $articulDesc)
         производятся сравнения со всеми комплектациями (массив $complectations). Если описание применяемости для запчасти (таблица catalog, столбец REC3)
          и комплектация не совпадают, то комплектация не попадает в выдачу
          * */
 
+
+
         foreach ($complectations as $indexC => $valueC) {
 
-
-            foreach ($articulDesc as $index => $value) {
-                $ct = 0;
-                $schemaOptions = $this->model()->multiexplode(array('+', ' +', '+ '), $value['REC3']);
+            $ct = 0;
+            $schemaOptions = $this->model()->multiexplode(array(' +'), $aAllarticulDesc);
 
 
-                foreach ($schemaOptions as $item) {
-
-                    $item = trim($item, ('*()'));
-                    if (strpos($item, ".")) {
-                        $plus = explode('.', $item);
 
 
-                        if (count($plus) == count(array_intersect($plus, explode('.', $valueC->getOptions()['OPTION9'])))) {
-                            $ct = $ct + 1;
-                        }
+            foreach ($schemaOptions as $item) {
 
 
-                    } else {
 
-                        if (in_array($item, explode('.', $valueC->getOptions()['OPTION9']))) {
-                            $ct = $ct + 1;
-                        }
 
+
+                if (strpos($item, ".")) {
+
+                    $plus = explode('.', $item);
+
+
+                    $plusExplode = $this->model()->multiexplode(array('.','+'), $item);
+
+
+
+                    foreach ($plusExplode as &$item)
+                    {
+                        $item = trim($item, ('*()'));
                     }
 
 
-                }
+
+                    if (count($plus) === count(array_intersect($plusExplode, explode('.', $valueC->getOptions()['OPTION9'])))) {
+                        $ct = $ct + 1;
+                    }
 
 
-                if ($ct === 0) {
-                    $oContainer->getActiveModification()->removeComplectation($indexC);
+                } else {
+
+                    if (in_array($item, explode('.', $valueC->getOptions()['OPTION9']))) {
+                        $ct = $ct + 1;
+                    }
+
                 }
+
 
             }
+
+
+            if ($ct === 0) {
+                $oContainer->getActiveModification()->removeComplectation($indexC);
+            }
+
+
         }
 
 
         return $oContainer;
     }
-
-
 
 } 
