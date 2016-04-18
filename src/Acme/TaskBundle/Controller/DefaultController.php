2c7cd9125ead70f32a5a8949223a317095d0b93b
denis <mail@gengen.by>
2016-04-18 23:45:00 +0300

    вин готов для всех случаев

--- src/Acme/TaskBundle/Controller/DefaultController.php
+++ src/Acme/TaskBundle/Controller/DefaultController.php
@@ -9,7 +9,9 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $marks = array('Huyndai', 'KIA', 'Honda', 'Suzuki', 'Subaru', 'Mazda', 'Mercedes', 'Smart', 'BMW', 'Mini', 'RollsRoyce', 'Saab', 'Audi', 'Volkswagen',
-            'Seat', 'Skoda', 'HondaEurope', 'Fiat', 'FiatProfessional', 'Lancia', 'AlfaRomeo', 'Abarth', 'Nissan', 'Infiniti');
+            'Seat', 'Skoda', 'HondaEurope', 'Fiat', 'FiatProfessional', 'Lancia', 'AlfaRomeo', 'Abarth', 'Nissan', 'Infiniti', 'LandRover');
+        sort($marks);
+        reset($marks);
         return $this->render('AcmeTaskBundle:Default:index.html.twig', array('marks' => $marks));
     }
 }
