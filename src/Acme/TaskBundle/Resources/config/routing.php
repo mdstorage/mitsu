41b2439418d06806cd928e478b4735531fcf4e0c
mdstorage <mail@gengen.by>
2016-09-28 18:24:40 +0300

    общий поиск по вин + https

--- src/Acme/TaskBundle/Resources/config/routing.php
+++ src/Acme/TaskBundle/Resources/config/routing.php
@@ -5,8 +5,12 @@ use Symfony\Component\Routing\Route;
 
 $collection = new RouteCollection();
 
-$collection->add('acme_task_homepage', new Route('/', array(
+$collection->add('acme_task_homepage', new Route('/',
+    array(
     '_controller' => 'AcmeTaskBundle:Default:index',
-)));
+),
+    array(
+        '_scheme' => 'https'
+    )));
 
 return $collection;
