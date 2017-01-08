364bd67a31c554e2579267e9368c981e88ae7099
denis <mail@gengen.by>
2017-01-08 23:23:22 +0300

    alfa vin

--- src/Catalog/AlfaRomeoBundle/Models/AlfaRomeoVinModel.php
+++ src/Catalog/AlfaRomeoBundle/Models/AlfaRomeoVinModel.php
@@ -19,8 +19,11 @@ class AlfaRomeoVinModel extends AlfaRomeoCatalogModel {
 
         $sql = "
         SELECT vin_chassis.mvs, motor, vin_chassis.date, mvs.cat_cod, catalogues.cat_dsc, comm_modgrp.cmg_dsc, brands.title, comm_modgrp.cmg_cod, mvs.mvs_dsc
-        FROM vin_chassis, mvs, catalogues, comm_modgrp, brands
-        WHERE (vin_chassis.vin = :vin or (vin_chassis.chassy = RIGHT(:vin,8) AND SUBSTRING(vin_chassis.mvs, 1, 3) = SUBSTRING(:vin, 4, 3)))
+        FROM vin_chassis, mvs, catalogues, comm_modgrp, brands, vin
+        WHERE (vin_chassis.vin = :vin or (vin_chassis.chassy = RIGHT(:vin,8) AND SUBSTRING(vin_chassis.mvs, 1, 3) = SUBSTRING(:vin, 4, 3))
+        OR (vin_chassis.chassy = RIGHT(:vin,8) AND vin.vin_cod = SUBSTRING(:vin, 4, 3)
+        AND SUBSTRING(vin_chassis.mvs, 1, 3) = vin.mod_cod)
+        )
         AND mvs.mod_cod = SUBSTRING(vin_chassis.mvs, 1, 3)
         AND mvs.mvs_version = SUBSTRING(vin_chassis.mvs, 4, 3)
         AND mvs.mvs_serie = SUBSTRING(vin_chassis.mvs, 7, 1)
@@ -35,26 +38,29 @@ class AlfaRomeoVinModel extends AlfaRomeoCatalogModel {
         $query->bindValue('vin', $vin);
         $query->execute();
 
-        $aData = $query->fetch();
-
+        $aData = $query->fetchAll();
 
 
         $result = array();
 
         if ($aData) {
-            $result = array(
-                'brand' => $aData['title'],
-                'model' => $aData['cmg_dsc'],
-                'modif' => $aData['cat_dsc'],
-                'modif_for_group' => $aData['cat_cod'],
-                'model_for_group' => $aData['cmg_cod'],
-                Constants::PROD_DATE => $aData['date'],
-                'region' => 'EU',
-                'motor' => $aData['motor'],
-                'mvs_dsc' => $aData['mvs_dsc']
+            foreach ($aData as $index => $value)
+            {
+                $result[Constants::PROD_DATE]=$value['date'];
+                $result[$index] = array(
+                    'brand' => $value['title'],
+                    'model' => $value['cmg_dsc'],
+                    'modif' => $value['cat_dsc'],
+                    'modif_for_group' => $value['cat_cod'],
+                    'model_for_group' => $value['cmg_cod'],
+                    Constants::PROD_DATE => $value['date'],
+                    'region' => 'EU',
+                    'motor' => $value['motor'],
+                    'mvs_dsc' => $value['mvs_dsc']
                 );
-        }
+            }
 
+        }
 
         return $result;
     }
